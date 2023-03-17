56e9d7c57f7afe4e63f719f54d64a0f0490ad315
akki <akhil.blueprism@gmail.com>
2023-03-17 11:00:51 +0530

    mavenproject

---
+++ src/main/java/in/civil/App.java
@@ -0,0 +1,13 @@
+package in.civil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
